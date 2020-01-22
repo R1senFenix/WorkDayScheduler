b0a732fc38f7c4826e4bd490e1edf573bcb35b89
RisenFenix <jjstram@gmail.com>
2020-01-21 23:16:03 -0600

    have started to add int the basic fomatting

---
+++ javascriptFolder/scheduleObject.js
@@ -0,0 +1,12 @@
+// unintialaized js object
+var dailyObject = {
+    Hour9th: { hourName: "9 AM", Events: "" },
+    Hour10th: { hourName: "10 AM", Events: "" },
+    Hour11th: { hourName: "11 AM", Events: "" },
+    Hour12th: { hourName: "12 AM", Events: "" },
+    Hour13th: { hourName: "1 PM", Events: "" },
+    Hour14th: { hourName: "2 PM", Events: "" },
+    Hour15th: { hourName: "3 PM", Events: "" },
+    Hour16th: { hourName: "4 PM", Events: "" },
+    Hour17th: { hourName: "5 PM", Events: "" },
+}
